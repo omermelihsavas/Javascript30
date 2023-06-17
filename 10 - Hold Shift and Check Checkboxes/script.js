90d0a45b1cc0e743f44fe1deb383d14f31220c6c
Omer Melih Savas <omermelihsavas@outlook.com>
2023-06-18 02:58:05 +0300

    Finished tenth project

---
+++ 10 - Hold Shift and Check Checkboxes/script.js
@@ -0,0 +1,22 @@
+const checkboxes = document.querySelectorAll('.card input[type="checkbox"]');
+
+let lastChecked;
+
+function handleClick(e) {
+    let inBetween = false;
+    if(e.shiftKey && this.checked) {
+        checkboxes.forEach(checkbox => {
+            if(checkbox === this || checkbox === lastChecked) {
+                inBetween = !inBetween;
+            }
+
+            if(inBetween) {
+                checkbox.checked = true;
+            }
+        });
+    }
+
+    lastChecked = this;
+};
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleClick));
